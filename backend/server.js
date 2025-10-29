7997801bac303f99eb29e086f925423d488f9666
AshraSingh <ashrajsingh9@gmail.com>
2025-10-29 18:50:38 +0530

    installed cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(express.json())
-app.use(cors(({ origin: 'https://balanceo.onrender.com' })));
+app.use(cors(({origin: 'http://localhost:5173'})));
 
 app.use('/api', logInRoutes)
 app.use('/api', signInRoutes)
