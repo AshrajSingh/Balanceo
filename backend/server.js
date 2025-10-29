eb567873bbdcc4360c25181d414887712859c42a
AshraSingh <ashrajsingh9@gmail.com>
2025-10-29 19:57:22 +0530

    installed dependencies

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(express.json())
-app.use(cors(({origin: 'http://localhost:5173'})));
+app.use(cors(({origin: ['http://localhost:5173', 'https://balanceo-fhz1.onrender.com']})));
 
 app.use('/api', logInRoutes)
 app.use('/api', signInRoutes)
